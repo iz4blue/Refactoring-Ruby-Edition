3974df0309886f69ee6813ef57acf02417419473
Joseph Kim <iz4blue@gmail.com>
2017-06-18 19:20:09 +0900

    p29 frequent_renter_point 변수를 삭제하고 메서드로 분리하다.

--- chapter-01/chapter-01.rb
+++ chapter-01/chapter-01.rb
@@ -53,20 +53,15 @@ class Customer
   end
 
   def statement
-    frequent_renter_points = 0, 0
     result = "고객 #{@name}의 대여 기록\n"
     @rentals.each do |element|
-
-      # 적립 포인트를 더함
-      frequent_renter_points += element.frequent_renter_points
-
       # 이번 대여의 계산 결과를 표시
       result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
     end
 
     # 푸터 행 추가
     result += "대여료는 #{total_charge}입니다.\n"
-    result += "적립 포인트는 #{frequent_renter_points}입니다."
+    result += "적립 포인트는 #{total_frequent_renter_points}입니다."
     result
   end
 
@@ -74,5 +69,9 @@ class Customer
 
   def total_charge
     @rentals.inject(0) { |sum, rental| sum + rental.charge }
+  end
+
+  def total_frequent_renter_points
+    @rentals.inject(0) { |sum, rental| sum + rental.frequent_renter_points }
   end
 end
